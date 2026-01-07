bb880ff181003c7b7a84a1e73fade1f4263b1eeb
Maggievira87 <margarethjvr87@gmail.com>
2026-01-07 15:01:57 -0500

    Proyecto Spring Boot inicial

---
+++ src/test/java/com/example/SistemabasicodeBlog/SistemabasicodeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemabasicodeBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemabasicodeBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
